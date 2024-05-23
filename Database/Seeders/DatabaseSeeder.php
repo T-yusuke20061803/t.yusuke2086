d63f8a5f684f118006571bee95ebb11f9acd1073
T-yusuke20061803 <t.yusuke0618@icloud.com>
2024-05-24 02:04:51 +0900

    dev_basis05

--- Database/Seeders/DatabaseSeeder.php
+++ Database/Seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeders;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(PostSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
